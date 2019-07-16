864812c987f86fd956ae0b02631c796bb615866e
Tutq268 <tutran00831@gmail.com>
2019-07-16 21:30:47 +0700

    [1.0.0] Add static pages.

---
+++ config/viewEngine.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import expressEjsExtend from 'express-ejs-extend'
+
+// cấu hình view engine cho app
+// import css from './../public'
+let configViewEngine = (app) => {
+  app.use(express.static("./public"))
+  app.engine("ejs", expressEjsExtend)
+  app.set("view engine","ejs")
+  app.set("views","./views")
+}
+
+module.exports = configViewEngine
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // let express = require('express')
 import express from "express"
 import monggoDB from './config/connectDB'
-import ContactModel from './models/contact.model'
+// import ContactModel from './models/contact.model'
+
+import configViewEngine from './config/viewEngine'
+
 require('dotenv').config()
 let app = express()
 
@@ -10,20 +13,29 @@ let app = express()
 monggoDB()
 
 
-app.get("/test-database",async (req,res)=> {
-    try {
-     let item = {
-         userId: '123456',
-         contactId: '194321439432'
-     }
-    //  let contact = await ContactModel.createNew(item)
-    let contact = await ContactModel.createNew(item)
-     res.send(contact)
-    }catch(err){
-        console.log(err)
-    }
-})
+// config viewengine
+configViewEngine(app)
 
+// app.get("/test-database",async (req,res)=> {
+//     try {
+//      let item = {
+//          userId: '654321',
+//          contactId: '194321439432'
+//      }
+//     //  let contact = await ContactModel.createNew(item)
+//     let contact = await ContactModel.createNew(item)
+//      res.send(contact)
+//     }catch(err){
+//         console.log(err)
+//     }
+// })
+app.get("/",(req,res)=>{
+   return res.render("main/master")
+})
+app.get("/login-register",(req,res)=>{
+    return res.render("auth/loginRegister")
+
+})
 app.listen(process.env.APP_PORT,(req,res)=> {
     console.log(`hello tu tran. I'm runiing at : ` + process.env.APP_PORT)
 })
