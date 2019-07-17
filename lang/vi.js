f4eb599a073a79b204c28f5c45408911169a6a62
Tutq268 <tutran00831@gmail.com>
2019-07-17 22:36:03 +0700

    [1.0.0] register user part 01

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect : "Email phải có định dang example@gmail.com",
+  gender_incorrect : "ủa, tại sao giới tính lại bị sai?",
+  password_incorrect : "Password phải có ít nhất 8 kí tư, bao gồm chữ hoa, số và kí tự đăck biệt",
+  password_confirmation_incorrect : "Nhập lại sai mật khẩu"
+}
